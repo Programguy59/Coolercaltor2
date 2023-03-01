dcaf6f5c221657d9fc22411b22f038709cecfeb0
This is a good commit <magn8244@elev.techcollege.dk>
2023-03-01 09:11:29 +0100

    added a single space for readability

--- Coolercaltor/Program.cs
+++ Coolercaltor/Program.cs
@@ -5,13 +5,14 @@ namespace Coolercaltor
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {         
             Console.Write("please enter first number: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("please enter second number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
             Console.Write("what operation would you like to perform now, choose + - * or /: ");
             string type = Console.ReadLine();
+
             if(type == "+")
             {
                 Console.WriteLine(Plus(num1,num2));
